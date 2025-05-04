9af65def5c4bb812590e76e220062e3f8a0bf113
el1ght <el1ghtifyoucan@gmail.com>
2025-05-04 19:31:08 +0300

    added algorithms ui

--- src/components/MatrixEditor.tsx
+++ src/components/MatrixEditor.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent,  CardFooter, } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
@@ -13,6 +13,33 @@ import {
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
+
+  import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+  } from "@/components/ui/chart"
+
+  import { TrendingUp } from "lucide-react"
+  import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+
+  const chartData = Array.from({ length: 100 }, (_, i) => ({
+    index: i + 1,
+    desktop: Math.floor(Math.random() * 300) + 50, // random value between 50 and 349
+    mobile: Math.floor(Math.random() * 200) + 20,  // random value between 20 and 219
+  }))
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#dddddd",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#000000",
+    },
+  } satisfies ChartConfig
 
 export default function MatrixEditor() {
   const [matrix, setMatrix] = useState<string[][]>([
@@ -90,41 +117,9 @@ export default function MatrixEditor() {
                 </div>
             </div>
 
-            <div className="flex flex-col gap-2">
-                <Dialog>
-                <DialogTrigger asChild>
-                <Button onClick={addRow}>Algorithm 1</Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px] min-w-[700px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-                </Dialog>
-                
-                <Button onClick={addRow}>Algorithm 2</Button>
+
             </div>
-        </div>
+
      
 
       <Card className="w-full overflow-auto">
@@ -294,6 +289,229 @@ export default function MatrixEditor() {
 
        
       </Card>
+
+      <Card className="w-full overflow-auto">
+      <CardHeader>
+          <CardTitle>Алгоритм 1</CardTitle>
+          <CardDescription>Enter numeric values in the matrix below.</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+
+
+          <div className="mt-6 max-w-sm flex gap-2">
+              <div className="flex gap-2 items-center max-w-30">
+              
+                <Input type="text" id="input" placeholder="Input"  />
+
+              </div>
+          </div>
+        </CardContent>
+
+      </Card>
+
+      <Card className="w-full overflow-auto">
+      <CardHeader>
+          <CardTitle>Алгоритм 2</CardTitle>
+          <CardDescription>Enter numeric values in the matrix below.</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+
+
+          <div className="mt-6 max-w-2xl flex gap-2">
+              <div className="flex gap-2 items-center max-w-30">
+              
+                <Input type="text" id="input" placeholder="Input"  />
+
+              </div>
+
+              <div className="flex gap-2 items-center max-w-30">
+              
+              <Input type="text" id="input" placeholder="Input"  />
+
+            </div>
+
+            <div className="flex gap-2 items-center max-w-30">
+              
+              <Input type="text" id="input" placeholder="Input"  />
+
+            </div>
+
+            <div className="flex gap-2 items-center max-w-30">
+              
+              <Input type="text" id="input" placeholder="Input"  />
+
+            </div>
+
+            <div className="flex gap-2 items-center max-w-30">
+              
+              <Input type="text" id="input" placeholder="Input"  />
+
+            </div>
+
+            <div className="flex gap-2 items-center max-w-30">
+              
+              <Input type="text" id="input" placeholder="Input"  />
+
+            </div>
+          </div>
+        </CardContent>
+
+      </Card>
+
+      <Card className="w-full overflow-auto">
+        <div className="mx-6 max-w-2xl flex-col gap-2">
+                <div className="flex">
+                    <Button>Solve</Button>
+                </div>
+
+                <h1 className="mt-6 mb-4">Алгоритм 1</h1>
+
+                <div className="inline-block">
+            <div className="grid" style={{ gridTemplateColumns: `120px repeat(${matrix[0]?.length || 0}, 1fr) 50px`, gap: '0.5rem' }}>
+              <div></div>
+              {columnLabels.map((label, index) => (
+                <div key={`col-wrap-${index}`} className="flex items-center gap-2">
+                  <Input value={label} disabled onChange={e => handleColumnLabelChange(index, e.target.value)} />
+                </div>
+              ))}
+              <div></div>
+              {matrix.map((row, rowIndex) => (
+                <>
+                  <div key={`row-wrap-${rowIndex}`} className="flex items-center gap-2">
+                    <Input value={rowLabels[rowIndex]} disabled onChange={e => handleRowLabelChange(rowIndex, e.target.value)} />
+    
+                  </div>
+                  {row.map((value, colIndex) => (
+                    <Input
+                      key={`${rowIndex}-${colIndex}`}
+                      type="text"
+                      placeholder="0"
+                      value={value}
+                      disabled
+                      onChange={e => handleChange(rowIndex, colIndex, e.target.value)}
+                    />
+                  ))}
+                  <div></div>
+                </>
+              ))}
+            </div>
+          </div>
+
+
+            <div className="flex gap-2 mt-6 items-center max-w-30">
+          
+                <Input type="text" disabled id="output" placeholder="Output"  />
+   
+            </div>
+
+            <h1 className="mt-6 mb-4">Алгоритм 2</h1>
+
+                <div className="inline-block">
+            <div className="grid" style={{ gridTemplateColumns: `120px repeat(${matrix[0]?.length || 0}, 1fr) 50px`, gap: '0.5rem' }}>
+              <div></div>
+              {columnLabels.map((label, index) => (
+                <div key={`col-wrap-${index}`} className="flex items-center gap-2">
+                  <Input value={label} disabled onChange={e => handleColumnLabelChange(index, e.target.value)} />
+                </div>
+              ))}
+              <div></div>
+              {matrix.map((row, rowIndex) => (
+                <>
+                  <div key={`row-wrap-${rowIndex}`} className="flex items-center gap-2">
+                    <Input value={rowLabels[rowIndex]} disabled onChange={e => handleRowLabelChange(rowIndex, e.target.value)} />
+    
+                  </div>
+                  {row.map((value, colIndex) => (
+                    <Input
+                      key={`${rowIndex}-${colIndex}`}
+                      type="text"
+                      placeholder="0"
+                      value={value}
+                      disabled
+                      onChange={e => handleChange(rowIndex, colIndex, e.target.value)}
+                    />
+                  ))}
+                  <div></div>
+                </>
+              ))}
+            </div>
+          </div>
+
+
+            <div className="flex gap-2 mt-6 items-center max-w-30">
+          
+                <Input type="text" disabled id="output" placeholder="Output"  />
+   
+            </div>
+
+
+        </div>
+
+      </Card>
+
+      <Card>
+      <CardHeader>
+        <CardTitle>Area Chart - Stacked</CardTitle>
+        <CardDescription>
+          Showing total visitors for the last 6 months
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="index"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<h1>Hey</h1>}
+            />
+            <Area
+              dataKey="mobile"
+              type="natural"
+              fill="var(--color-mobile)"
+              fillOpacity={0.4}
+              stroke="var(--color-mobile)"
+              stackId="a"
+            />
+            <Area
+              dataKey="desktop"
+              type="natural"
+              fill="var(--color-desktop)"
+              fillOpacity={0.4}
+              stroke="var(--color-desktop)"
+              stackId="a"
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="flex items-center gap-2 leading-none text-muted-foreground">
+              January - June 2024
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
     </div>
   )
 }
